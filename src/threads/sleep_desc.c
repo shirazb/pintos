a17849930f9625a8538cf418170898d960766904
Shiraz Butt <sb4515@ic.ac.uk>
2017-01-10 16:37:01 +0000

    Implemented non-busy waiting timer_sleep().

---
+++ src/threads/sleep_desc.c
@@ -0,0 +1,61 @@
+#include "sleep_desc.h"
+#include <lib/debug.h>
+
+static void wake_up (struct sleep_desc *desc);
+static bool has_overslept (struct sleep_desc *desc, int64_t ticks);
+
+/**
+ * Initialises a thread_sleep_desc. Semaphore is initialised with 0. Adds thread
+ * to sleeping_threads list.
+ */
+void
+sleep_desc_init (struct sleep_desc *desc, int64_t wake_time, struct list *sleeping_threads)
+{
+  ASSERT (desc != NULL);
+
+  desc->wake_time = wake_time;
+  list_push_back (sleeping_threads, &desc->sleep_elem);
+//  printf ("---------- DEBUG: Added thread %d to the list.\n", t->tid);
+  sema_init(&desc->sema, 0);
+//  printf ("---------- DEBUG: Initd the semaphore for thread %d.\n", t->tid);
+}
+
+/**
+ * If ticks exceeds the threads wake time, wakes the thread up
+ */
+void
+sleep_desc_wake_if_overslept(struct sleep_desc *desc, int64_t ticks)
+{
+  if (has_overslept (desc, ticks))
+    {
+      wake_up (desc);
+    }
+}
+
+/**
+ * Wakes a thread (by upping its semaphore). Only to be called by
+ * wake_if_overslept().
+ * @param t Thread to wake.
+ */
+static void
+wake_up (struct sleep_desc *desc)
+{
+  ASSERT (desc != NULL);
+  sema_up (&desc->sema);
+  list_remove (&desc->sleep_elem);
+  desc = NULL;
+
+  // printf ("---------- DEBUG: Removed thread %d from the list.\n", t->tid);
+}
+
+/**
+ * has_overslept(t) <-> ticks has now exceeded t->wake_time.
+ * @param t Thread whose sleep time is being checked.
+ * @return If ticks now exceeds t->wake_time.
+ */
+static bool
+has_overslept (struct sleep_desc *desc, int64_t ticks)
+{
+  ASSERT (desc != NULL);
+  return ticks >= desc->wake_time;
+}
---
+++ src/threads/sleep_desc.h
@@ -0,0 +1,28 @@
+#ifndef PINTOS_TASK0_SB4515_SLEEP_DESC_H
+#define PINTOS_TASK0_SB4515_SLEEP_DESC_H
+
+#include <list.h>
+#include <stdint.h>
+#include <threads/synch.h>
+
+/* Contains info for implementing the sleeping of threads without busy waiting,
+   used in timer_sleep() and timer_interrupt().
+
+   Factoring this out allows it to be kept in the sleep timer's stack frame.
+
+   Require access to the semaphore to sleep / wake the thread.
+   Require access to wake_time to know when to wake the thread.
+   Require access to the list_elem so thread.c can maintain a list of sleeping
+     threads.
+*/
+struct sleep_desc
+{
+    struct semaphore sema;           /* Used to sleep the thread */
+    int64_t wake_time;               /* Scheduled waking up time */
+    struct list_elem sleep_elem;     /* List element for sleeping_threads */
+};
+
+void sleep_desc_init (struct sleep_desc *desc, int64_t wake_time, struct list *sleeping_threads);
+void sleep_desc_wake_if_overslept (struct sleep_desc *desc, int64_t ticks);
+
+#endif //PINTOS_TASK0_SB4515_SLEEP_DESC_H
