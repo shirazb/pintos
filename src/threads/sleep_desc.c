55a4d85a7729c1ea92a445a095fc7849d2b3b9c0
Shiraz Butt <shiraz.b@icloud.com>
2017-01-10 18:38:50 +0000

    Used ordered list for sleeping_threads to reduce make about to be woken threads be at the front of the list, reducing the time the PIT handler takes

--- src/threads/sleep_desc.c
+++ src/threads/sleep_desc.c
@@ -3,6 +3,7 @@
 
 static void wake_up (struct sleep_desc *desc);
 static bool has_overslept (struct sleep_desc *desc, int64_t ticks);
+static list_less_func order_by_wake_time;
 
 /**
  * Initialises a thread_sleep_desc. Semaphore is initialised with 0. Adds thread
@@ -17,7 +18,7 @@ sleep_desc_init (struct sleep_desc *desc, int64_t wake_time, struct list *sleepi
   sema_init(&desc->sema, 0);
 
   enum intr_level old_level = intr_set_level(INTR_OFF);
-  list_push_back (sleeping_threads, &desc->sleep_elem);
+  list_insert_ordered (sleeping_threads, &desc->sleep_elem, order_by_wake_time, NULL);
   intr_set_level(old_level);
 }
 
@@ -57,4 +58,20 @@ has_overslept (struct sleep_desc *desc, int64_t ticks)
 {
   ASSERT (desc != NULL);
   return ticks >= desc->wake_time;
+}
+
+/**
+ * Orders two sleep_descs by their wait time, first to wake up first.
+ */
+static bool
+order_by_wake_time (const struct list_elem *a,
+                    const struct list_elem *b,
+                    void *aux UNUSED)
+{
+  struct sleep_desc *s1 = list_entry(a, struct sleep_desc, sleep_elem);
+  struct sleep_desc *s2 = list_entry(b, struct sleep_desc, sleep_elem);
+
+  ASSERT (s1 != NULL && s2 != NULL);
+
+  return s1->wake_time <= s2->wake_time;
 }
