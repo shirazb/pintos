aed9837b3bd083e1a238024e1068c7f717c93c29
dr_spaceship <thomas.price15@imperial.co.uk>
2017-01-29 06:19:24 +0000

    implemented fixed point numbers and added to makefile

---
+++ src/threads/fixed-point.c
@@ -0,0 +1,50 @@
+
+
+#include "threads/fixed-point.h"
+
+inline fixed_point_t CAST_INT_TO_FP(int n) {
+    return n * F;
+}
+inline int CAST_FP_TO_INT_ROUND_ZERO(fixed_point_t n) {
+    return n / F;
+}
+
+inline int CAST_FP_TO_INT_ROUND_NEAREST(fixed_point_t n) {
+    if (n >= 0) {
+        return (n + F/2) / F;
+    } else {
+        return (n - F/2) / F;
+    }
+}
+
+inline fixed_point_t ADD_FP_FP(fixed_point_t n1, fixed_point_t n2) {
+    return n1 + n2;
+}
+
+inline fixed_point_t ADD_FP_INT(fixed_point_t n1, int n2) {
+    return n1 + (n2 * F);
+}
+
+inline fixed_point_t SUB_FP_FP(fixed_point_t n1, fixed_point_t n2) {
+    return n1 - n2;
+}
+
+inline fixed_point_t SUB_FP_INT(fixed_point_t n1, int n2) {
+    return n1 - (n2 * F);
+}
+
+inline fixed_point_t MUL_FP_FP(fixed_point_t n1, fixed_point_t n2) {
+    return (((int64_t) n1) * n2) / F;
+}
+
+inline fixed_point_t MUL_FP_INT(fixed_point_t n1, int n2) {
+    return n1 * n2;
+}
+
+inline fixed_point_t DIV_FP_FP(fixed_point_t n1, fixed_point_t n2) {
+    return (((int64_t) n1) * F )/ n2;
+}
+
+inline fixed_point_t DIV_FP_INT(fixed_point_t n1, int n2) {
+    return n1 / n2;
+}
