377623a74893c7777dcd4999067bfb5a47e87136
Shiraz Butt <sb4515@ic.ac.uk>
2017-02-02 22:24:45 +0000

    [sb][tp] Changed fixed point function declarations in headers to be extern inline, forcing them to be inlined so they have similar performance to macros.

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -5,20 +5,20 @@
 
 typedef int32_t fixed_point_t;
 
-fixed_point_t CAST_INT_TO_FP(int n);
-int CAST_FP_TO_INT_ROUND_ZERO(fixed_point_t n);
-int CAST_FP_TO_INT_ROUND_NEAREST(fixed_point_t n);
+extern inline fixed_point_t CAST_INT_TO_FP(int n);
+extern inline int CAST_FP_TO_INT_ROUND_ZERO(fixed_point_t n);
+extern inline int CAST_FP_TO_INT_ROUND_NEAREST(fixed_point_t n);
 
-fixed_point_t ADD_FP_FP(fixed_point_t n1, fixed_point_t n2);
-fixed_point_t ADD_FP_INT(fixed_point_t n1, int n2);
+extern inline fixed_point_t ADD_FP_FP(fixed_point_t n1, fixed_point_t n2);
+extern inline fixed_point_t ADD_FP_INT(fixed_point_t n1, int n2);
 
-fixed_point_t SUB_FP_FP(fixed_point_t n1, fixed_point_t n2);
-fixed_point_t SUB_FP_INT(fixed_point_t n1, int n2);
+extern inline fixed_point_t SUB_FP_FP(fixed_point_t n1, fixed_point_t n2);
+extern inline fixed_point_t SUB_FP_INT(fixed_point_t n1, int n2);
 
-fixed_point_t MUL_FP_FP(fixed_point_t n1, fixed_point_t n2);
-fixed_point_t MUL_FP_INT(fixed_point_t n1, int n2);
+extern inline fixed_point_t MUL_FP_FP(fixed_point_t n1, fixed_point_t n2);
+extern inline fixed_point_t MUL_FP_INT(fixed_point_t n1, int n2);
 
-fixed_point_t DIV_FP_FP(fixed_point_t n1, fixed_point_t n2);
-fixed_point_t DIV_FP_INT(fixed_point_t n1, int n2);
+extern inline fixed_point_t DIV_FP_FP(fixed_point_t n1, fixed_point_t n2);
+extern inline fixed_point_t DIV_FP_INT(fixed_point_t n1, int n2);
 
 #endif //PINTOS_36_FIXED_POINT_H
