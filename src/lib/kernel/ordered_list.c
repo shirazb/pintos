8f0a7a15bfe2a9a9995c4b2cf0febd447b5d12ee
sb4515 <sb4515@ic.ac.uk>
2017-02-23 17:10:39 +0000

    [sb][ts][tp][dg] Added ordered_list files

---
+++ src/lib/kernel/ordered_list.c
@@ -0,0 +1,65 @@
+#include <lib/debug.h>
+#include <stdio.h>
+#include "ordered_list.h"
+
+
+void
+ordered_list_init(struct ordered_list *ol, list_less_func *less, void *aux) {
+    ASSERT(ol != NULL);
+    ASSERT(less != NULL);
+
+    list_init(&ol->list);
+    ol->less = less;
+    ol->aux  = aux;
+    ol->size = 0;
+}
+
+/**
+ * Given an ordered_list whose single list_elem has become out of order,
+ * puts it back in order by removal then ordered insertion.
+ */
+// TODO: WRITE IN DESIGN DOC EXPLAINING WHY REMOVE/INSERT IS FASTER THAN SORT
+void
+ordered_list_reinsert(struct ordered_list *ol, struct list_elem *elem) {
+    ASSERT(ol != NULL);
+
+    struct list_elem *removed = list_remove(elem);
+    ASSERT(removed != NULL);
+    list_insert_ordered(&ol->list, elem, ol->less, ol->aux);
+}
+
+void
+ordered_list_resort(struct ordered_list *ol) {
+    ASSERT(ol != NULL);
+    list_sort(&ol->list, ol->less, NULL);
+}
+
+void
+ordered_list_insert(struct ordered_list *ol, struct list_elem *elem) {
+    ASSERT(ol != NULL);
+    ol->size++;
+    list_insert_ordered(&ol->list, elem, ol->less, ol->aux);
+}
+
+struct list_elem *
+ordered_list_front(struct ordered_list *ol) {
+    ASSERT(ol != NULL);
+    return list_front(&ol->list);
+}
+
+struct list_elem *
+ordered_list_pop_front(struct ordered_list *ol) {
+    ASSERT(ol != NULL);
+    ol->size--;
+    return list_pop_front(&ol->list);
+}
+
+bool ordered_list_empty(struct ordered_list *ol) {
+    ASSERT(ol != NULL);
+    return list_empty(&ol->list);
+}
+
+
+int ordered_list_size(struct ordered_list *ol) {
+    return ol->size;
+}
