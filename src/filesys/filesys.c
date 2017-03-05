bb7f659a124ae793ce5472894a1d74ec144cbbea
dr_spaceship <thomas.price15@imperial.co.uk>
2017-03-05 02:12:32 +0000

    [tp] file system uses read/write lock for concurrent opening of files

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -11,73 +11,77 @@
 /* Partition that contains the file system. */
 struct block *fs_device;
 
-static struct lock filesys_lock;
+static struct read_write_lock filesys_lock;
 
-static inline void lock_filesys (void);
-static inline void release_filesys (void);
-
-static void do_format (void);
+static void do_format(void);
 
 static inline void
-lock_filesys (void) {
-  lock_acquire (&filesys_lock);
+lock_filesys_read(void) {
+    r_lock_acquire(&filesys_lock);
 }
 
 static inline void
-release_filesys (void) {
-  lock_release (&filesys_lock);
+release_filesys_read(void) {
+    r_lock_release(&filesys_lock);
+}
+
+static inline void
+lock_filesys_write(void) {
+    w_lock_acquire(&filesys_lock);
+}
+
+static inline void
+release_filesys_write(void) {
+    w_lock_release(&filesys_lock);
 }
 
 /* Initializes the file system module.
    If FORMAT is true, reformats the file system. */
 void
-filesys_init (bool format) 
-{
-  lock_init (&filesys_lock);
+filesys_init(bool format) {
+    rw_lock_init(&filesys_lock);
 
-  fs_device = block_get_role (BLOCK_FILESYS);
-  if (fs_device == NULL)
-    PANIC ("No file system device found, can't initialize file system.");
+    fs_device = block_get_role(BLOCK_FILESYS);
+    if (fs_device == NULL)
+        PANIC ("No file system device found, can't initialize file system.");
 
-  inode_init ();
-  free_map_init ();
+    inode_init();
+    free_map_init();
 
-  if (format) 
-    do_format ();
+    if (format)
+        do_format();
 
-  free_map_open ();
+    free_map_open();
 }
 
 /* Shuts down the file system module, writing any unwritten data
    to disk. */
 void
-filesys_done (void) 
-{
-  free_map_close ();
+filesys_done(void) {
+    free_map_close();
 }
-
+
 /* Creates a file named NAME with the given INITIAL_SIZE.
    Returns true if successful, false otherwise.
    Fails if a file named NAME already exists,
    or if internal memory allocation fails. */
 bool
-filesys_create (const char *name, off_t initial_size) 
-{
-  lock_filesys ();
+filesys_create(const char *name, off_t initial_size) {
+    lock_filesys_write();
 
-  block_sector_t inode_sector = 0;
-  struct dir *dir = dir_open_root ();
-  bool success = (dir != NULL
-                  && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size)
-                  && dir_add (dir, name, inode_sector));
-  if (!success && inode_sector != 0) 
-    free_map_release (inode_sector, 1);
-  dir_close (dir);
+    block_sector_t inode_sector = 0;
+    struct dir *dir = dir_open_root();
+    bool success = (dir != NULL
+                    && free_map_allocate(1, &inode_sector)
+                    && inode_create(inode_sector, initial_size)
+                    && dir_add(dir, name, inode_sector));
+    if (!success && inode_sector != 0)
+        free_map_release(inode_sector, 1);
+    dir_close(dir);
 
-  release_filesys ();
+    release_filesys_write();
 
-  return success;
+    return success;
 }
 
 /* Opens the file with the given NAME.
@@ -86,23 +90,21 @@ filesys_create (const char *name, off_t initial_size)
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
 struct file *
-filesys_open (const char *name)
-{
-  lock_filesys ();
+filesys_open(const char *name) {
+    lock_filesys_read();
 
-  struct dir *dir = dir_open_root ();
-  struct inode *inode = NULL;
+    struct dir *dir = dir_open_root();
+    struct inode *inode = NULL;
 
+    if (dir != NULL)
+        dir_lookup(dir, name, &inode);
+    dir_close(dir);
 
-  if (dir != NULL)
-    dir_lookup (dir, name, &inode);
-  dir_close (dir);
+    struct file *opened_file = file_open(inode);
 
-  struct file *opened_file = file_open (inode);
+    release_filesys_read();
 
-  release_filesys ();
-
-  return opened_file;
+    return opened_file;
 }
 
 /* Deletes the file named NAME.
@@ -110,27 +112,25 @@ filesys_open (const char *name)
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
 bool
-filesys_remove (const char *name) 
-{
-  lock_filesys ();
+filesys_remove(const char *name) {
+    lock_filesys_write();
 
-  struct dir *dir = dir_open_root ();
-  bool success = dir != NULL && dir_remove (dir, name);
-  dir_close (dir); 
+    struct dir *dir = dir_open_root();
+    bool success = dir != NULL && dir_remove(dir, name);
+    dir_close(dir);
 
-  release_filesys ();
+    release_filesys_write();
 
-  return success;
+    return success;
 }
-
+
 /* Formats the file system. */
 static void
-do_format (void)
-{
-  printf ("Formatting file system...");
-  free_map_create ();
-  if (!dir_create (ROOT_DIR_SECTOR, 16))
-    PANIC ("root directory creation failed");
-  free_map_close ();
-  printf ("done.\n");
+do_format(void) {
+    printf("Formatting file system...");
+    free_map_create();
+    if (!dir_create(ROOT_DIR_SECTOR, 16))
+        PANIC ("root directory creation failed");
+    free_map_close();
+    printf("done.\n");
 }
