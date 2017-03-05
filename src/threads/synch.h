eee669eb71c67fdb75c93c5411ecffacdcfe9d5a
dr_spaceship <thomas.price15@imperial.co.uk>
2017-03-05 01:07:05 +0000

    [tp] implemented a sick read/write lock.

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -5,41 +5,70 @@
 #include <stdbool.h>
 
 /* A counting semaphore. */
-struct semaphore 
-  {
+struct semaphore {
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
-  };
+};
 
-void sema_init (struct semaphore *, unsigned value);
-void sema_down (struct semaphore *);
-bool sema_try_down (struct semaphore *);
-void sema_up (struct semaphore *);
-void sema_self_test (void);
+void sema_init(struct semaphore *, unsigned value);
+
+void sema_down(struct semaphore *);
+
+bool sema_try_down(struct semaphore *);
+
+void sema_up(struct semaphore *);
+
+void sema_self_test(void);
 
 /* Lock. */
-struct lock 
-  {
+struct lock {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-  };
+};
 
-void lock_init (struct lock *);
-void lock_acquire (struct lock *);
-bool lock_try_acquire (struct lock *);
-void lock_release (struct lock *);
-bool lock_held_by_current_thread (const struct lock *);
+void lock_init(struct lock *);
+
+void lock_acquire(struct lock *);
+
+bool lock_try_acquire(struct lock *);
+
+void lock_release(struct lock *);
+
+bool lock_held_by_current_thread(const struct lock *);
+
+/*
+ * Readers-writer lock that prevents the readers from starving the writers and vice versa.
+ * Note: a writer can still starve if the writers aren't woken up in FIFO.
+ */
+struct read_write_lock {
+    unsigned int num_readers;
+    struct lock num_readers_access;
+    struct lock resource_access;
+    struct lock wait_for_service;
+};
+
+void rw_lock_init(struct read_write_lock *rw_lock);
+
+void r_lock_acquire(struct read_write_lock *rw_lock);
+
+void r_lock_release(struct read_write_lock *rw_lock);
+
+void w_lock_acquire(struct read_write_lock *rw_lock);
+
+void w_lock_release(struct read_write_lock *rw_lock);
 
 /* Condition variable. */
-struct condition 
-  {
+struct condition {
     struct list waiters;        /* List of waiting semaphore_elems. */
-  };
+};
 
-void cond_init (struct condition *);
-void cond_wait (struct condition *, struct lock *);
-void cond_signal (struct condition *, struct lock *);
-void cond_broadcast (struct condition *, struct lock *);
+void cond_init(struct condition *);
+
+void cond_wait(struct condition *, struct lock *);
+
+void cond_signal(struct condition *, struct lock *);
+
+void cond_broadcast(struct condition *, struct lock *);
 
 /* Optimization barrier.
 
