3e88bc2619bb1d8aaf3bb97d8348d4c2352d7103
tp1515 <thomas.price15@imperial.ac.uk>
2017-03-10 17:23:18 +0000

    [tp][ts][dg] started suppl_page_table

--- src/vm/suppl_page.c
+++ src/vm/suppl_page.c
@@ -1,2 +1,27 @@
 
-//TODO
+#include <vm/suppl_page.h>
+#include <lib/debug.h>
+
+
+void sp_table_init(struct sp_table *sp_table) {
+    hash_init(&sp_table->pages, NULL, NULL, NULL);
+}
+
+size_t get_page_num(struct sp_table *page_table, void *addr) {
+
+    // pages.get(addr)
+    struct page_num_entry search_entry;
+    search_entry.addr = addr;
+    struct hash_elem *e = hash_find(&page_table->pages, &search_entry.hash_elem);
+
+    // TODO: check that this doesn't happen
+    if (!e) {
+        NOT_REACHED();
+    }
+
+    struct page_num_entry *entry = hash_entry(e, struct page_num_entry, hash_elem);
+
+    return entry->page_num;
+}
+
+
--- src/vm/suppl_page.h
+++ src/vm/suppl_page.h
@@ -1,6 +1,22 @@
-//TODO
 
 #ifndef PINTOS_36_SUPPL_PAGE_H
 #define PINTOS_36_SUPPL_PAGE_H
+
+#include <stddef.h>
+#include <hash.h>
+
+struct sp_table {
+    struct hash pages; // map<page_addr, page_num_entry>
+};
+
+struct page_num_entry {
+    size_t page_num;
+    void *addr;
+    struct hash_elem hash_elem;
+};
+
+void sp_table_init(struct sp_table *sp_table);
+
+size_t get_page_num(struct sp_table *page_table, void *addr);
 
 #endif //PINTOS_36_SUPPL_PAGE_H
