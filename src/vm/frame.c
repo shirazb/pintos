85e6e67c8787578f11815e9c5553f92fc993243c
tp1515 <thomas.price15@imperial.ac.uk>
2017-03-10 16:09:43 +0000

    [tp][ts][sb][dg] mostly implemented frame_alloc_page and frame_free_page

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,5 +1,23 @@
-void frame_table_init() {
 
+#include <threads/malloc.h>
+#include <vm/frame.h>
+#include <threads/palloc.h>
+
+static struct frame_table table;
+static struct read_write_lock frame_table_rw_lock;
+
+
+static struct frame_info *make_frame_info(size_t page_num, size_t index);
+
+static hash_hash_func frame_hash_func;
+static hash_less_func frame_less_func;
+
+
+void frame_table_init() {
+    table.used_frames = bitmap_create(TABLE_SIZE);
+    rw_lock_init(&frame_table_rw_lock);
+    // TODO: DO
+    hash_init(&table.frames, NULL, NULL, NULL);
 }
 
 void frame_table_deinit() {
@@ -8,4 +26,69 @@ void frame_table_deinit() {
 
 void frame_table_evict() {
 
+}
+
+void *frame_alloc_page(size_t page_num) {
+    w_lock_acquire(&frame_table_rw_lock);
+    size_t free_index = bitmap_scan_and_flip(table.used_frames, 0, 1, false);
+    w_lock_release(&frame_table_rw_lock);
+
+    struct frame_info *entry;
+
+    if (free_index == BITMAP_ERROR) {
+        // TODO: Eviction
+        NOT_REACHED();
+    }
+    entry = make_frame_info(page_num, free_index);
+
+    w_lock_acquire(&frame_table_rw_lock);
+    hash_insert(&table.frames, &entry->hash_elem);
+    w_lock_release(&frame_table_rw_lock);
+
+    return entry->vaddr_page;
+}
+
+struct frame_info *make_frame_info(size_t page_num, size_t index) {
+    struct frame_info *entry = malloc(sizeof(struct frame_info));
+    entry->page_num = page_num;
+    entry->tid = thread_current()->tid;
+    entry->vaddr_page = palloc_get_page(PAL_USER);
+    entry->index = index;
+    return entry;
+}
+
+void frame_free_page(tid_t tid, size_t page_num) {
+    // find the frame information mapped from the process page table
+    struct frame_info search_info;
+    search_info.tid = tid;
+    search_info.page_num = page_num;
+
+    w_lock_acquire(&frame_table_rw_lock);
+    struct hash_elem *e = hash_delete(&table.frames, &search_info.hash_elem);
+
+    // TODO: freeing a swapped out page should clear the swap table of the entry
+    if (!e) {
+        NOT_REACHED();
+    }
+
+    struct frame_info *entry = hash_entry(e, struct frame_info, hash_elem);
+
+    // set the bit for the frame to unallocated
+    bitmap_flip(table.used_frames, entry->index);
+    w_lock_release(&frame_table_rw_lock);
+
+    // free the page in virtual memory and the entry
+    palloc_free_page(entry->vaddr_page);
+    free(entry);
+}
+
+// frame hashing function using the process ID (tid) and the page number
+unsigned frame_hash_func(const struct hash_elem *e, void *aux UNUSED) {
+    return NULL;
+}
+
+bool frame_less_func(const struct hash_elem *a,
+                             const struct hash_elem *b,
+                             void *aux) {
+    return false;
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -6,20 +6,29 @@
 #include "threads/thread.h"
 
 #define NUM_FRAMES ((2 << 21) - 1)
+#define TABLE_SIZE (4 << 20)
 
 struct frame_info {
     tid_t tid;
-    int page_num;
+    size_t page_num;
+    void * vaddr_page;
+    size_t index;
+    struct hash_elem hash_elem;
 };
 
 struct frame_table {
-    struct frame_info frames[NUM_FRAMES];
+    struct hash frames; // Map<frame_index, frame_info>
     struct bitmap *used_frames;
 };
 
+// TODO: make private?
 void frame_table_init();
 void frame_table_deinit();
 void frame_table_evict();
+
+
+void * frame_alloc_page(size_t page_num);
+void frame_free_page(tid_t tid, size_t page_num);
 
 
 #endif //PINTOS_36_FRAME_H
