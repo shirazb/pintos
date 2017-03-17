4c63725f98c56a1f65d53f46acf0feeb6e367f2e
Shiraz Butt <sb4515@ic.ac.uk>
2017-03-17 17:30:01 +0000

    [sb][tp] Refactored in swap.c

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,10 +1,3 @@
-// TODO:
-/*  Helper to allocate new swap space for new entry
- *  Helper to free swap space for deleted entry
- *
- *  Main to swap a frame and swap entry
- */
-
 #include "swap.h"
 #include <lib/kernel/bitmap.h>
 #include <threads/synch.h>
